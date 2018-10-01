323c1308534e96686259bbc55bfb8dfccc66d261
bboodhoo119 <boodhoob@sheridancollege.ca>
2018-10-01 00:36:50 -0400

    routing in progress of being fixed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,8 +10,14 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { InventoryModule } from './inventory/inventory.module';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { RegisterComponent } from "./register/register.component";
-import { SidebarModule } from "./sidebar/sidebar.module";
 import { AppRoutingModule } from "./app.routing.module";
+import { TokenExtraction } from "./helpers";
+import { SidebarModule } from "./sidebar/sidebar.module";
+import { FixedPluginModule } from './shared/fixedplugin/fixedplugin.module';
+import { FooterModule } from './shared/footer/footer.module';
+import { NavbarModule } from './shared/navbar/navbar.module';
+import { AdminLayoutComponent } from './layouts/admin/admin-layout.component';
+import { AuthLayoutComponent } from './layouts/auth/auth-layout.component';
 
 @NgModule({
   declarations: [
@@ -19,17 +25,22 @@ import { AppRoutingModule } from "./app.routing.module";
     DashboardComponent,
     LoginPageComponent,
     RegisterComponent,
+    AdminLayoutComponent,
+    AuthLayoutComponent
   ],
   imports: [
     AppRoutingModule,
     BrowserModule,
-    InventoryModule,
     SidebarModule,
+    InventoryModule,
     RouterModule,
     HttpModule,
     HttpClientModule,
+    FixedPluginModule,
+    FooterModule,
+    NavbarModule,
   ],
-  providers: [],
+  providers: [TokenExtraction],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -6,28 +6,34 @@ import { ShowComponent } from "./inventory/show/show.component";
 import { LoginPageComponent } from "./login-page/login-page.component";
 import { RegisterComponent } from "./register/register.component";
 import { SidebarComponent } from "./sidebar/sidebar.component";
+import { AdminLayoutComponent } from './layouts/admin/admin-layout.component';
 
 const routes: Routes = [
     {
+    path: '',
+    component: AdminLayoutComponent,
+    children: [{
         path: '',
         component: DashboardComponent
-    },
-    {
-        path: 'login',
-        component: LoginPageComponent
-    },
-    {
-        path: 'register',
-        component: RegisterComponent
-    },
-    {
-        path: 'add',
-        component: AddComponent
-    },
-    {
-        path: 'show',
-        component: ShowComponent
-    }
+        },
+        {
+            path: 'login',
+            component: LoginPageComponent
+        },
+        {
+            path: 'register',
+            component: RegisterComponent
+        },
+        {
+            path: 'add',
+            component: AddComponent
+        },
+        {
+            path: 'show',
+            component: ShowComponent
+        }
+
+]}
 ]
 
 @NgModule({
