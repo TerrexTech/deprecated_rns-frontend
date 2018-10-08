26c6777da694847fc0989e8b4340886ddf59f27a
bboodhoo119 <boodhoob@sheridancollege.ca>
2018-10-08 00:22:49 -0400

    basic css working with datatable

--- src/app/data-table/data-table.component.ts
+++ src/app/data-table/data-table.component.ts
@@ -1,38 +1,63 @@
-import { Component, OnDestroy, OnInit, Input } from '@angular/core';
+import { Component, OnDestroy, OnInit, Input, ViewChild, ElementRef } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import { Subject } from 'rxjs';
-import { Employee } from '../models/employee';
 import 'rxjs/add/operator/map';
+
+declare interface DataTable {
+  headerRow: string[];
+  footerRow: string[];
+  dataRows: string[][];
+}
 
 @Component({
   selector: 'app-data-table',
   templateUrl: './data-table.component.html',
   styleUrls: ['./data-table.component.css']
 })
+
 export class DataTableComponent implements OnInit {
 
-  dtOptions: DataTables.Settings = {};
   data: any[] = [];
   // We use this trigger because fetching the list of persons can be quite long,
   // thus we ensure the data is fetched before rendering
-  dtTrigger: Subject<any> = new Subject();
+  @ViewChild('table') table: any
   @Input() fields: any
   @Input() properties: any
+  public dataTable: DataTable;
+
   constructor(private http: Http) { }
 
   ngOnInit(): void {
-    this.dtOptions = {
-      pagingType: 'full_numbers',
-      pageLength: 2    };
     this.http.get('./assets/mock_data2.JSON')
       .map(this.extractData)
       .subscribe(data => {
         this.data = data;
         console.log(data)
-        // Calling the DT trigger to manually render the table
-        this.dtTrigger.next();
       });
+
+    this.dataTable = {
+      headerRow: this.fields,
+      footerRow: this.fields,
+      dataRows: this.data
+    };
   }
+
+  ngAfterViewInit(){
+    this.table.DataTable({
+      "pagingType": "full_numbers",
+      "lengthMenu": [
+        [10, 25, 50, -1],
+        [10, 25, 50, "All"]
+      ],
+      responsive: true,
+      language: {
+        search: "_INPUT_",
+        searchPlaceholder: "Search records",
+      }
+
+    });
+  }
+
 
   private extractData(res: Response) {
     const body = res.json();
